178fb6b33f497be5aa9910fb55674f60c54498f7
dyzania <117496046+dyzania@users.noreply.github.com>
2024-06-18 01:34:35 +0800

    f

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "hospitalms";
+    $dbname = "hospitalemes";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
